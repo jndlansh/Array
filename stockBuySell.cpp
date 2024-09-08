bcd1d25e909f47eee1faa726b5689808fe3a282f
jndlansh <jndl.ansh@gmail.com>
2024-09-08 12:59:05 +0530

    Create stockBuySell.cpp

---
+++ stockBuySell.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxi=0;
+        int n=prices.size();
+        int mini=INT_MAX;
+        for(int i=0;i<n;i++){
+            mini=min(mini,prices[i]);
+            maxi=max(maxi,prices[i]-mini);            
+        }
+        return maxi;
+        
+    }
+};
